aaab888de0fd63c3227c64ff81543c9e5d1157d2
bsorrentino <bartolomeo.sorrentino@gmail.com>
2023-01-31 16:18:03 +0100

    refactor: clean code

--- packages/youtube-downloader/zx-youtube-download.mjs
+++ packages/youtube-downloader/zx-youtube-download.mjs
@@ -14,9 +14,22 @@ if( !url ) {
     url = await question( 'youtube url: ')
 }
 
+/*
+itag container quality codecs                 bitrate  audio bitrate
+18   mp4       360p    avc1.42001E, mp4a.40.2 696.66KB 96KB
+137  mp4       1080p   avc1.640028            4.53MB
+248  webm      1080p   vp9                    2.52MB
+136  mp4       720p    avc1.4d4016            2.2MB
+247  webm      720p    vp9                    1.44MB
+135  mp4       480p    avc1.4d4014            1.1MB
+134  mp4       360p    avc1.4d401e            593.26KB
+140  mp4               mp4a.40.2              128KB
+*/
 const info  = await ytdl.getInfo( url ) 
+
+// let format = ytdl.chooseFormat(info.formats, { quality: '136' });
+// console.log('Format found!', format);
 
 const title = info.videoDetails.title ?? path.basename(url)
 
-ytdl.downloadFromInfo(info)
-  .pipe(fs.createWriteStream(`${title}.mp4`));
+ytdl.downloadFromInfo(info).pipe(fs.createWriteStream(`${title}.mp4`));
